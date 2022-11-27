adf7104c4b6af10b5091239369b9a5d65f53c0d8
Gercogor <gercogor@gmail.com>
2022-11-27 17:15:23 +0700

    Updates

---
+++ bundle.js
@@ -0,0 +1,52 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+// import 'normalize.css';
+
+
+
+
+
+
+function updateTextInput(value) {
+    document.getElementById('output-range').value = value + '%';
+}
+
+document.querySelector('.form__input_file').addEventListener('change', function () {
+    let file = this.files[0];
+    document.querySelector('.form__output_file').innerHTML = file.name;
+});
+
+const wrapper = document.querySelector('.form__select-wrapper');
+const lists = document.querySelectorAll('.form__select-options li');
+const selectButton = document.querySelector('.form__select-button');
+
+const body = document.querySelector('body')
+
+let options = [];
+
+function updateSelectName(event) {
+    lists.forEach(item => item.classList.remove('selected'))
+    event.target.classList.add('selected')
+    selectButton.firstElementChild.innerText = event.target.innerText;
+    wrapper.classList.remove('active')
+}
+
+lists.forEach(item => {
+    item.addEventListener('click', updateSelectName)
+    options.push(item.innerHTML)
+});
+
+selectButton.addEventListener("click", () => {
+    wrapper.classList.toggle('active');
+});
+
+
+const burgerButton = document.querySelector('.burger__header')
+const navBurger = document.querySelector('.navigation__burger')
+
+burgerButton.addEventListener('click', ()=>{
+    navBurger.classList.toggle('active');
+})
+/******/ })()
+;
